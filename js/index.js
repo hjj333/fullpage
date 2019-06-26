5c88ef7fed0f3815d09aafa3f36c63b85b66f63a
hjj <476265397@qq.com>
2019-06-26 22:52:27 +0800

    fullpage

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.fullpage').fullpage({
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCenterde: false,
+        navigation: true,
+    });
+})
